c8d38a38d94064595f516b20df77a81d034a921f
llynn <lexielynn1@gmail.com>
2014-12-06 21:23:06 +0100

    Initial Commit

---
+++ EatYourFeelings/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EatYourFeelings
+//
+//  Created by A. Lynn on 12/6/14.
+//  Copyright (c) 2014 Lexie Lynn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
